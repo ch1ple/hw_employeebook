2a9f8661fb1ddab5225474d2bc7997e2134b8ab7
ch1ple <teavis@mail.ru>
2024-09-19 23:41:14 +0300

    initial commit

---
+++ src/main/java/me/ch1ple/hw/employeebook/Application.java
@@ -0,0 +1,13 @@
+package me.ch1ple.hw.employeebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
